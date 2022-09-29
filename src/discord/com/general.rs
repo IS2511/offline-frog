b94b5a971c3218681757aa32ff8bafa397f757d4
IS2511 <is2511@bk.ru>
2022-09-29 20:58:23 +0300

    feat(discord): update `help` with up-to-date info

--- src/discord/com/general.rs
+++ src/discord/com/general.rs
@@ -26,23 +26,25 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
 
     macro_rules! cmd {
         ($com:expr, $des:expr) => {
-            format!("`{}{}` - {}\n", prefix, $com, $des)
+            format!("`{}{}`\t - \t{}\n", prefix, $com, $des).as_str()
         }
     }
     macro_rules! cmd_list {
         ($($com:expr),*) => {
             {
                 let mut s = String::new();
-                $( s.push_str($com.as_str()); )*
+                $( s.push_str($com); )*
+                // s.replace("\t", "⠀") // Blank braille character
                 s
             }
         };
     }
 
+    // TODO: Rework this to use clap? Maybe when clap updates?
     msg.channel_id.send_message(ctx, |m| {
         m.embed(|e| {
             e.title("Help");
-            e.description("Me when help lol");
+            e.description("Me when help lol\n\nWhy is this text styled so bad? I don't know, I'm not a designer.\nDiscord eats whitespaces and I don't want to make the whole thing a code block :shrug:");
             e.fields(vec![
                 ("General",
                  cmd_list!(
@@ -51,13 +53,16 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
                      cmd!("about", "About this bot")
                  ), false),
                 ("Channel", cmd_list!(
-                     cmd!("channel add <channel>", "Add channel to watchlist"),
-                     cmd!("channel remove <channel>", "Remove channel from watchlist"),
+                     cmd!("channel add <channels>", "Add channels to watchlist"),
+                     cmd!("channel remove <channels>", "Remove channels from watchlist"),
                      cmd!("channel list", "List all channels in watchlist")
                  ), false),
                 ("Trigger", cmd_list!(
-                     cmd!("trigger add <trigger>", "Add plaintext match trigger (ex: your username)"),
-                     cmd!("trigger remove <id>", "Remove trigger with specified id"),
+                     cmd!("trigger add <trigger>", "Add plaintext match trigger (ex: \"AzureDiamond\")"),
+                    "`\t-r, --regex`\tAdd regex match trigger (ex: *todo*)\n",
+                    "`\t           `\tThe regex flavour is Rust, see [here](https://docs.rs/regex/latest/regex/#syntax)\n",
+                    "`\t-c, --case-sensitive`\tMatch case-sensitive (default: case-insensitive)\n",
+                     cmd!("trigger remove <ids>", "Remove triggers with specified ids"),
                      cmd!("trigger list", "List all triggers and their ids")
                  ), false),
             ]);
